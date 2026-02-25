5534e240b958d81143ad95e355574766694d3bd2
Radko Stefani <radko.stefani@student.tuke.sk>
2026-02-25 12:34:28 +0100

    Matrix

--- matrixexercise.py
+++ matrixexercise.py
@@ -7,3 +7,15 @@ A = np.array([[1,2],[3,4]])
 B = np.array([[5,6],[7,8]])
 
 print(matrix_addition(A,B))
+
+def matrix_multi(A,B):
+    return A@B
+
+def matrix_transpo(A,B):
+    return A.T
+
+def matrix_det(A,B):
+    return np.linalg.det(A)
+
+def matrix_inverse(A,B):
+    return np.linalg.inv(A)
